3a76bf01aa64585d31be96e05216ebe58104e4ab
Steve <stevedsimkins@gmail.com>
2024-03-20 21:09:18 -0400

    init

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,119 @@
-import Image from "next/image";
+import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar"
 
-export default function Home() {
+async function cronFeed(channel: any, nextPage: any, pageSize: any) {
+  try {
+    const result = await fetch(
+      `https://api.pinata.cloud/v3/farcaster/casts?channel=${channel}&pageSize=${pageSize}&pageToken=${nextPage}`, {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${process.env.PINATA_JWT}`
+      }
+    });
+    if (!result.ok) {
+      throw new Error("failed to fetch data")
+    }
+    const resultData = await result.json();
+    return resultData
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+}
+
+const dummyCasts = [
+  {
+    fid: 20591,
+    hash: '0x4fa1c5f37ba1d663e225d67c7268523ddfc7e9c7',
+    short_hash: '0x4fa1c5f3',
+    thread_hash: '0x66d56e0e5d587cd576b05b861f0b6e35a946f322',
+    parent_hash: '0x66d56e0e5d587cd576b05b861f0b6e35a946f322',
+    parent_url: null,
+    root_parent_url: 'https://warpcast.com/~/channel/pinata',
+    parent_author: {
+      uid: 4823,
+      fid: 4823,
+      custody_address: '0x7f9a6992a54dc2f23f1105921715bd61811e5b71',
+      recovery_address: '0x00000000fcb080a4d6c39a9354da9eb9bc104cd7',
+      following_count: 848,
+      follower_count: 14863,
+      verifications: [Array],
+      bio: 'Writer. Building @pinatacloud. Tinkering with a Farcaster native alternative to GoodReads: https://readcast.xyz \\ https://polluterofminds.com',
+      display_name: 'Justin Hunter',
+      pfp_url: 'https://i.seadn.io/gae/lhGgt7yK1JiBVYz_HBxcAmYLRtP03aw5xKX4FgmFT9Ai7kLD5egzlLvb0lkuRNl28shtjr07DC8IHzLUkTqlWUMndUzC9R5_MSxH3g?w=500&auto=format',
+      username: 'polluterofminds'
+    },
+    author: {
+      uid: 20591,
+      fid: 20591,
+      custody_address: '0x62402434701e0ce0ae4ea4b3caf68230a6ddbe43',
+      recovery_address: '0x00000000fcb080a4d6c39a9354da9eb9bc104cd7',
+      following_count: 13,
+      follower_count: 261,
+      verifications: [Array],
+      bio: 'Everyone is from somewhere. Cofounder and CEO of Pinata. https://www.pinata.cloud/farcaster',
+      display_name: 'Kyle Tut',
+      pfp_url: 'https://i.imgur.com/TLMFnH6.jpg',
+      username: 'kyletut'
+    },
+    content: "What's more degen then shilling lol",
+    timestamp: '2024-03-07T16:16:27Z',
+    embeds: [],
+    reactions: { likes: [Array], recasts: [Array] },
+    replies: { count: 1 },
+    mentioned_profiles: []
+  },
+  {
+    fid: 4823,
+    hash: '0x66d56e0e5d587cd576b05b861f0b6e35a946f322',
+    short_hash: '0x66d56e0e',
+    thread_hash: null,
+    parent_hash: null,
+    parent_url: null,
+    root_parent_url: 'https://warpcast.com/~/channel/pinata',
+    parent_author: null,
+    author: {
+      uid: 4823,
+      fid: 4823,
+      custody_address: '0x7f9a6992a54dc2f23f1105921715bd61811e5b71',
+      recovery_address: '0x00000000fcb080a4d6c39a9354da9eb9bc104cd7',
+      following_count: 848,
+      follower_count: 14863,
+      verifications: [Array],
+      bio: 'Writer. Building @pinatacloud. Tinkering with a Farcaster native alternative to GoodReads: https://readcast.xyz \\ https://polluterofminds.com',
+      display_name: 'Justin Hunter',
+      pfp_url: 'https://i.seadn.io/gae/lhGgt7yK1JiBVYz_HBxcAmYLRtP03aw5xKX4FgmFT9Ai7kLD5egzlLvb0lkuRNl28shtjr07DC8IHzLUkTqlWUMndUzC9R5_MSxH3g?w=500&auto=format',
+      username: 'polluterofminds'
+    },
+    content: "This is the signal! You know what to do. If you're playing  today or if you are a dev that wants to use the Pinata Farcaster API, like your thumbs off. 👍🏻❤️  This cast needs to get at least 624 likes to unlock the Farcaster API docs. https://pinatadrops.com/apps/farcaster-api",
+    timestamp: '2024-03-07T16:10:48Z',
+    embeds: [[Object]],
+    reactions: { likes: [Array], recasts: [Array] },
+    replies: { count: 8 },
+    mentioned_profiles: [[Object]]
+  }
+]
+
+export default async function Home() {
+
+  const feed = await cronFeed("https://warpcast.com/~/channel/pinata", "", 100)
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex min-h-screen flex-col items-center justify-start gap-12 mt-12">
+      {feed.data.casts.map((cast: any) =>
+        <div className="flex gap-4 w-[500px] flex-row items-start" key={cast.hash}>
+          <Avatar>
+            <AvatarImage src={cast.author.pfp_url} />
+              <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+          <div className="flex flex-col items-start">
+            <div className="flex gap-2">
+              <p className="font-bold">{cast.author.display_name}</p>
+              <p className="text-gray-600">@{cast.author.username}</p>
+            </div>
+            <p>{cast.content}</p>
+          </div>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      )}
     </main>
   );
 }
